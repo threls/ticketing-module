ddb0e37e182dfc07f10dfc948ce1d18b472e7b85
sabina1997 <41695168+sabina1997@users.noreply.github.com>
2025-06-04 23:06:56 +0000

    Fix styling

--- src/Dto/BookingConfirmationMailDto.php
+++ src/Dto/BookingConfirmationMailDto.php
@@ -4,9 +4,7 @@ namespace Threls\ThrelsTicketingModule\Dto;
 
 use Carbon\Carbon;
 use Illuminate\Support\Collection;
-use Spatie\LaravelData\Attributes\MapName;
 use Spatie\LaravelData\Data;
-use Spatie\LaravelData\Mappers\SnakeCaseMapper;
 
 class BookingConfirmationMailDto extends Data
 {
