f4184fbd6e65e1bd5962d5b2d65bbb45e679dace
sabina <sabinahaloci@gmail.com>
2025-06-20 09:56:22 +0200

    fix puppeteer

--- src/Jobs/GenerateTicketPDFsJob.php
+++ src/Jobs/GenerateTicketPDFsJob.php
@@ -40,6 +40,7 @@ class GenerateTicketPDFsJob implements ShouldQueue
             $pdf = Pdf::withBrowsershot(function (Browsershot $browsershot) {
                 if (! app()->environment('local')) {
                     $browsershot
+                        ->setEnvironmentOptions(["XDG_CONFIG_HOME" => "/tmp/google-chrome-for-testing", "XDG_CACHE_HOME" => "/tmp/google-chrome-for-testing"])
                         ->setChromePath(config('ticketing-module.chrome_path')) // Use manually installed Chromium
                         ->setCustomTempPath(storage_path('temp'))    // Custom temp directory for server compatibility
                         ->noSandbox()                                // Disable sandbox for headless Chromium compatibility
