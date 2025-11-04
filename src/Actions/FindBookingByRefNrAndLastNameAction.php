ae5f9006d98c5c896e15f69a18a45b9cbc10b48c
ardit bleta <bletardit@gmail.com>
2025-11-04 09:52:55 +0100

    improve search in booking retrieval

--- src/Actions/FindBookingByRefNrAndLastNameAction.php
+++ src/Actions/FindBookingByRefNrAndLastNameAction.php
@@ -8,10 +8,21 @@ class FindBookingByRefNrAndLastNameAction
 {
     public function execute(string $referenceNr, string $lastName)
     {
-        return Booking::with('bookingClient')
+        $booking = Booking::with('bookingClient')
             ->where('reference_nr', $referenceNr)
             ->whereHas('bookingClient', function ($query) use ($lastName) {
                 $query->where('last_name', $lastName);
+            })
+            ->first();
+
+        if ($booking) {
+            return $booking;
+        }
+
+        return Booking::with('bookingClient')
+            ->where('reference_nr', 'LIKE', '%' . $referenceNr . '%')
+            ->whereHas('bookingClient', function ($query) use ($lastName) {
+                $query->where('last_name', 'LIKE', '%' . $lastName . '%');
             })
             ->firstOrFail();
     }
